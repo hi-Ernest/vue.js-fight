acd3b306b4df51d0f0426b0d6e2bf6b4ec93648d
ruihuachen <chenhr1130@163.com>
2020-02-11 23:56:31 +0800

    update shopping car

---
+++ vue.js-fight/vue-chatper5/shop-car.js
@@ -0,0 +1,51 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        index: '',
+        item: '',
+        list: [
+            {
+                id: 1,
+                name: 'iphone11',
+                price: 6999,
+                count: 55
+            },
+            {
+                id: 2,
+                name: 'ipad',
+                price: 2999,
+                count: 23
+            },
+            {
+                id: 3,
+                name: 'macbook pro',
+                price: 12999,
+                count: 90
+            }
+        ]
+    },
+    computed: {
+        totalPrice: function() {
+            var total = 0;
+            for(var i = 0; i < this.list.length; i++) {
+                total += this.list[i].price * this.list[i].count;
+            }
+            //千位分隔符
+            return total .toString () .replace(/\B (?= (\d{3}) +$)/g,','); 
+        }
+        
+    },
+    menthods: {
+        handleReduce: function(index) {
+            if(this.list[i].count === 1) return;
+            this.list[index].count--;
+        },
+        handleAdd: function(index) {
+            this.list[index].count++;
+        },
+        handleRemove: function(index) {
+            this.list[index].splice(index, 1);
+        }
+    }
+});
+
